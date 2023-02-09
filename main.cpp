f801ff6a3242916621d3d4eba5296a00c7636bf4
Paulius Širka <s047033@ad.viko.lt>
2023-02-09 12:23:27 +0200

    testavimas

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//using namespace std;
+int main(int a) {
+    std::cout << "Irasykite numeri!" << std::endl;
+    std::cin>> a;
+    std::cout <<"numeris: " <<a;
+    return 0;
+}
